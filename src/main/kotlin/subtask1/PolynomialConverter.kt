8ad57478fe2819cbd2a86d4b640f702410483f07
iliuha1907 <60312075+iliuha1907@users.noreply.github.com>
2020-05-06 20:05:24 +0300

    add Task1

--- src/main/kotlin/subtask1/PolynomialConverter.kt
+++ src/main/kotlin/subtask1/PolynomialConverter.kt
@@ -4,6 +4,36 @@ class PolynomialConverter {
 
     // TODO: Complete the following function
     fun convertToStringFrom(numbers: Array<Int>): String? {
-        throw NotImplementedError("Not implemented")
+       val sb = StringBuilder()
+        val last = numbers.size - 1
+        for (i in numbers.indices) {
+            if (numbers[i] == 0) {
+                continue
+            }
+            if (numbers[i] > 0) {
+                if (i != 0) {
+                    if (Math.abs(numbers[i]) == 1) {
+                        sb.append(" + ")
+                    } else {
+                        sb.append(" + " + Math.abs(numbers[i]))
+                    }
+                } else {
+                    sb.append(Math.abs(numbers[i]))
+                }
+            } else {
+                if (Math.abs(numbers[i]) == 1) {
+                    sb.append(" - ")
+                } else {
+                    sb.append(" - " + Math.abs(numbers[i]))
+                }
+            }
+            val deg = last - i
+            if (deg > 1) {
+                sb.append("x^$deg")
+            } else if (deg == 1) {
+                sb.append("x")
+            }
+        }
+        return if (sb.toString() == "") null else sb.toString()
     }
 }
